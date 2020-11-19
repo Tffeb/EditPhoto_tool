03774f30d5ec18daaac9fe58c2997a2cb76935c5
liubingsong <l>
2020-11-19 17:44:54 +0800

    init -am

--- src/pages/imgbar/index.js
+++ src/pages/imgbar/index.js
@@ -1,35 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { Upload, Modal } from 'antd'
+import React, { Component } from 'react'
+import { Upload, Modal, Form } from 'antd'
 import { PlusOutlined } from '@ant-design/icons';
 import './index.css'
 
-export default function App() {
-    const [previewVisible, setPreviewVisible] = useState(false)
-    const [previewTitle, setPreviewTitle] = useState('')
-    const [previewImage, setPreviewImage] = useState('')
-    const [fileList, setFileList] = useState([])
-    // 图片上传
-    // const uploadChange = e => {
-    //     if (Array.isArray(e)) {
-    //         return e.map(item => { item.status = 'done'; return item });
-    //     }
-    //     return e.fileList.map(item => { item.status = 'done'; return item })
-    // }
-    const collectFun = () => {
-        //收集图片dom
-        let images = document.querySelectorAll('.imgbar .ant-upload-list-item-image');
-        console.log(images,'images');
-        [].forEach.call(images, el => {
-            if (!el.attributes.draggable) {
-                el.setAttribute('draggable', true);
-                el.addEventListener('dragstart', e => {
-                    // e.dataTransfer.setData('imgData', JSON.stringify({ url: e.target.currentSrc }));
-                    console.log(e.target.currentSrc, 'ss')
-                });
-            }
-        })
+export default class App extends Component {
+    state = {
+        previewVisible: false,
+        previewTitle: '',
+        previewImage: '',
     }
-    function getBase64(file) {
+    form = Form.useForm();
+    // 图片上传
+    uploadChange = e => {
+        if (Array.isArray(e)) {
+            return e.map(item => { item.status = 'done'; return item });
+        }
+        return e.fileList.map(item => { item.status = 'done'; return item });
+    }
+    getBase64 = (file) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader();
             reader.readAsDataURL(file);
@@ -37,46 +25,77 @@ export default function App() {
             reader.onerror = error => reject(error);
         });
     }
-    // 关闭预览图片
-    const handleCancel = () => setPreviewVisible(false);
-    const handlePreview = async file => {
-        if (!file.url && !file.preview) {
-            file.preview = await getBase64(file.originFileObj);
-        }
-        setPreviewVisible(true)
-        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
-        setPreviewImage(file.url || file.preview)
-    };
+    componentDidUpdate() {
+        // const upload = this.props.form.getFieldValue('upload');
+        // console.log(upload)
 
-    const handleChange = ({ fileList }) => {
-        const list = fileList.map(item => { item.status = 'done'; return item });
-        collectFun();
-        setFileList(list);
+        // if (upload) {
+        //     this.getImgHandle()
+        // }
+    }
+    componentDidMount() {
+        console.log(this.props, 'props')
+    }
+    //收集图片dom
+    getImgHandle = () => {
+        let images = document.querySelectorAll('.imgbar .ant-upload-list-item-image');
+        console.log(images, 'images');
+        if (images.length > 0) {
+            [].forEach.call(images, el => {
+                if (!el.attributes.draggable) {
+                    el.setAttribute('draggable', true);
+                    el.addEventListener('dragstart', e => {
+                        // e.dataTransfer.setData('imgData', JSON.stringify({ url: e.target.currentSrc }));
+                        console.log(e.target.currentSrc, 'ss')
+                    });
+                }
+            })
+        }
+    }
+    handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await this.getBase64(file.originFileObj);
+        }
+        this.setState({
+            previewImage: file.url || file.preview,
+            previewTitle: file.name || file.url.substring(file.url.lastIndexOf('/') + 1),
+            previewVisible: true
+        })
     };
-    return (
-        <div className="imgbar">
-            <div className="upload_title">图片上传</div>
-            <Upload
-                accept="image/gif,image/png,image/jpg"
-                customRequest={() => { }}
-                listType="picture"
-                fileList={fileList}
-                onPreview={handlePreview}
-                onChange={handleChange}
-            >
-                <div>
-                    <PlusOutlined />
-                    <div className="ant-upload-text">上传</div>
-                </div>
-            </Upload>
-            <Modal
-                visible={previewVisible}
-                title={previewTitle}
-                footer={null}
-                onCancel={handleCancel}
-            >
-                <img alt="example" style={{ width: '100%' }} src={previewImage} />
-            </Modal>
-        </div>
-    )
+    render() {
+        console.log(this.form)
+        const { previewVisible, previewTitle, previewImage } = this.state;
+        return (
+            <div className="imgbar">
+                <div className="upload_title">图片上传</div>
+                <Form form={this.form}>
+                    <Form.Item
+                        name="upload"
+                        valuePropName="fileList"
+                        getValueFromEvent={this.uploadChange}
+                    >
+                        <Upload
+                            accept="image/gif,image/png,image/jpg"
+                            customRequest={() => { }}
+                            listType="picture"
+                            onPreview={this.handlePreview}
+                        >
+                            <div>
+                                <PlusOutlined />
+                                <div className="ant-upload-text">上传</div>
+                            </div>
+                        </Upload>
+                    </Form.Item>
+                </Form>
+                <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={() => this.setState({ previewVisible: false })}
+                >
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </div>
+        )
+    }
 }
